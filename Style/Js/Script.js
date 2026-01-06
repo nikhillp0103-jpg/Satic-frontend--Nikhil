08510435e55eeeb4cf71a8bad525040a5bdf0dce
nikhillp0103-jpg <nikhillp0103@gmail.com>
2026-01-06 16:40:56 +0530

    Update Script.js

--- Style/Js/Script.js
+++ Style/Js/Script.js
@@ -1,1 +1,7 @@
-console.log("Satic Day 1: JS linked correctly");
+console.log("Satic Day 2: Primary Button loaded");
+
+document.querySelectorAll('.primary-btn:not([disabled])').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert('Primary button clicked!');
+  });
+});
