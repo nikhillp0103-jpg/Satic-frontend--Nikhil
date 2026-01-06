15742de5a787a1bac02377e75b3c386b505eacfb
nikhillp0103-jpg <nikhillp0103@gmail.com>
2026-01-06 16:34:24 +0530

    Delete Day-2 directory

--- Day-2/Script.js
+++
@@ -1,7 +0,0 @@
-console.log("Satic Day 2: Primary Button loaded");
-
-document.querySelectorAll('.primary-btn:not([disabled])').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert('Primary button clicked!');
-  });
-});
