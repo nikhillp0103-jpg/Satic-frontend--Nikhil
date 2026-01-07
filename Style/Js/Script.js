4cc668fccaf7831201637ab75378b31b25b375e8
nikhillp0103-jpg <nikhillp0103@gmail.com>
2026-01-07 17:21:42 +0530

    Update Script.js

--- Style/Js/Script.js
+++ Style/Js/Script.js
@@ -1,7 +1,16 @@
-console.log("Satic Day 2: Primary Button loaded");
+console.log("Satic Day 3: Desktop Navbar loaded");
 
-document.querySelectorAll('.primary-btn:not([disabled])').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert('Primary button clicked!');
+
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    
+    document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
+    
+    e.target.classList.add('active');
   });
+});
+
+
+document.querySelector('.nav-cta').addEventListener('click', () => {
+  alert('Get Started clicked!');
 });
